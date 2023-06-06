7056570180943ccf2c5616c4a89eb448d515c2e6
Noah Cowan <nsncowan@gmail.com>
2023-06-06 14:19:14 -0700

    reusable form

--- src/components/ReusableForm.js
+++ src/components/ReusableForm.js
@@ -5,11 +5,18 @@ function ReusableForm(props) {
   return (
     <React.Fragment>
       <form onSubmit={props.formSubmissionHandler}>
-        <input/>
-        <input/>
-        <input/>
-        <input/>
-        <input/>
+        <input
+          type="text"
+          name="name"
+          placeholder="Item Name"/>
+        <input
+          type="text"
+          name="origin"
+          placeholder="Item Origin"/>
+        <input
+          type="number"
+          name="price"
+          placeholder="Item Price"/>
       </form>
     </React.Fragment>
   );
